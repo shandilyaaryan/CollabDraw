50424c83f6fb5c28714d2d9a3b68a3b00e60bd95
shandilyaaryan <aryanshandilya10@gmail.com>
2025-03-02 21:16:22 +0530

    Refactor backend scripts and add WebSocket server implementation

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -7,3 +7,5 @@ const port = 3001;
 app.get("/", (req, res) => {
   res.send("Hello Guys");
 });
+
+app.listen(port);
---
+++ apps/websocket-backend/src/index.ts
@@ -0,0 +1,13 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+
+  ws.on("message", (data) => {
+    console.log("recieved %s", data);
+  });
+
+  ws.send("something");
+});
