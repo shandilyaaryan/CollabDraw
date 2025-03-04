82bf7173d7c22eefda6c62652ea00a8d4d60fc4f
shandilyaaryan <aryanshandilya10@gmail.com>
2025-03-04 16:20:31 +0530

    Add JWT authentication with signup and signin endpoints in http-backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config.js";
+import { middleware } from "./middleware.js";
 
 const app = express();
 
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello Guys");
+app.post("/signup", (req, res) => {
+    // db call
+    res.json({
+        userId: "123"
+    })
+});
+
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign({ userId }, JWT_SECRET);
+  res.json({ token });
+});
+
+app.post("/room", middleware, (req, res) => {
+  //db call
+
+  res.json({
+    roomId: 123,
+  });
 });
 
 app.listen(port);
