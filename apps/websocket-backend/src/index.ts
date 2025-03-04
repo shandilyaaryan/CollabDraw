31903d633923b024b4f739398bf1ffc0a193bf19
shandilyaaryan <aryanshandilya10@gmail.com>
2025-03-04 16:35:18 +0530

    Add JWT authentication support to WebSocket server

--- apps/websocket-backend/src/index.ts
+++ apps/websocket-backend/src/index.ts
@@ -1,13 +1,27 @@
 import { WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on("connection", (ws) => {
-  ws.on("error", console.error);
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") ?? "";
+
+  const decoded = jwt.verify(token, JWT_SECRET);
+
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
 
   ws.on("message", (data) => {
-    console.log("recieved %s", data);
+    ws.send("pong");
   });
-
-  ws.send("something");
 });
